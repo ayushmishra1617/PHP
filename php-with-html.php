97f8e2008f8755ab655afb262719ae5ac3b6d30b
Ayush Mishra <145481735+ayushmishra1617@users.noreply.github.com>
2025-06-25 01:34:33 +0530

    Create php-with-html.php

---
+++ php-with-html.php
@@ -0,0 +1,25 @@
+//php-with-html
+//php inside html
+//html inside php
+
+<?php
+    echo "<h1 style= 'color:blue'>  PHP with HTML</h1>";
+    echo "<h2 style='color:red'> HTML with PHP</h2>";
+?>
+
+<?php
+$name="Ayush";
+echo "<h1 style='color:green'>My name is $name</h1>";
+?>
+
+<h1>
+    <?php
+    $name = "Mishra";
+    echo "My name is $name";
+    echo "<br>";
+
+    
+    
+    ?>
+
+</h1>
