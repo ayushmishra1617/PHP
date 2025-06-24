3ff9743b3031ec16d4494163c4fcbb598c704959
Ayush Mishra <145481735+ayushmishra1617@users.noreply.github.com>
2025-06-25 00:38:16 +0530

    Create variable.php

---
+++ variable.php
@@ -0,0 +1,38 @@
+<?php
+
+$name="Mishra";
+
+
+echo $name;
+echo"<br>";
+
+echo $name;
+echo"<br>";
+
+
+echo $name;
+echo"<br>";
+
+
+echo $name;
+echo"<br>";
+
+
+// Arithmetic Operations
+
+$num=5;
+
+echo $num+7;
+echo"<br>"; 
+
+echo $num*7;
+echo"<br>";
+
+echo $num-7;
+echo"<br>";
+
+echo $num/7;
+echo"<br>";
+
+
+?>
