dace3c46066760a4cb10b2547d750e232ad32a54
Ayush Mishra <145481735+ayushmishra1617@users.noreply.github.com>
2025-06-25 00:48:46 +0530

    Update variable.php
    Rules of Variable in php

--- variable.php
+++ variable.php
@@ -1,3 +1,14 @@
+//A variable starts with sign $.
+//A variable name cannot start with number
+//A variavle name must start with a letter or the underscore(_) character.
+//A variable name cannot start with special character 
+//A variable name can only contain alpha-numeric characters and underscore
+//$name,$Name, and $NAME all three are different
+// Variable syntax
+//$sname= "AYUSH"
+
+
+
 <?php
 
 $name="Mishra";
