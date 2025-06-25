306f598956e3c6d5e94a843f4eea33967480e7a8
Ayush Mishra <145481735+ayushmishra1617@users.noreply.github.com>
2025-06-26 02:53:05 +0530

    Create datatype.php

---
+++ datatype.php
@@ -0,0 +1,28 @@
+<?php
+
+$name="Ayush";
+echo var_dump($name);
+echo"</br>";
+
+
+$num=10;
+echo var_dump($num);
+echo"</br>";
+
+$numFloat=101.12;
+echo var_dump($numFloat);
+echo"</br>";
+
+$boolean=true;
+echo var_dump($boolean);
+echo"</br>";
+
+$arr=["Ayush","Mishra",203];
+echo var_dump($arr);
+echo"</br>";
+
+$nul=null;
+echo var_dump($nul);
+echo"</br>";
+
+?>
