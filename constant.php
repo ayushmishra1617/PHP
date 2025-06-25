f5a3e83b3a1204715f9ed013873bd3048f059b47
Ayush Mishra <145481735+ayushmishra1617@users.noreply.github.com>
2025-06-26 02:26:38 +0530

    Create constant.php

---
+++ constant.php
@@ -0,0 +1,11 @@
+<?php
+const name="AYush";
+//const name="Piyush";//this canot be declared again because it is already exist
+echo name; 
+?>
+
+<?php
+echo "</br>";
+define("dataa","I am learning php");
+echo dataa;
+?>
