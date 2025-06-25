f460d878512bf63b82549fe8107c4b2517acdf21
Ayush Mishra <145481735+ayushmishra1617@users.noreply.github.com>
2025-06-26 03:09:45 +0530

    Create arithematic.php

---
+++ arithematic.php
@@ -0,0 +1,30 @@
+<?php
+
+$a=20;
+$b=10;
+
+//Addition
+echo $a+$b;
+echo"</br>";
+
+//Substraction
+echo $a-$b;
+echo"</br>";
+
+//Multiplication
+echo $a*$b;
+echo"</br>";
+
+//Division
+echo $a/$b;
+echo"</br>";
+
+//Modulation
+echo $a%$b;
+echo"</br>";
+
+//Exponential
+echo $a**$b;
+echo"</br>";
+
+?>
