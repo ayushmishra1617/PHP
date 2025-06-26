dfb2647c913e215b9ec8344a02d1459da56a228f
Ayush Mishra <145481735+ayushmishra1617@users.noreply.github.com>
2025-06-26 12:35:18 +0530

    Create assignment.php

---
+++ assignment.php
@@ -0,0 +1,44 @@
+//+= → $x += 5; (adds 5 to $x)
+
+//-= → $x -= 5; (subtracts 5 from $x)
+
+//*= → $x *= 5; (multiplies $x by 5)
+
+//__/= → $x /= 5; (divides $x by 5)
+
+//%= → $x %= 5; (modulus of $x by 5)//
+
+<?php
+
+$a=10;
+$b=20;
+
+//$a=$a+$b;
+$a+=$b;
+echo $a;
+echo "</br>";
+
+$a-=$b;
+echo $a;
+echo "</br>";
+
+$a*=$b;
+echo $a;
+echo "</br>";
+
+$a/=$b;
+echo $a;
+echo "</br>";
+
+$a%=$b;
+echo $a;
+echo "</br>";
+
+$a**=$b;
+echo $a;
+echo "</br>";
+
+
+
+
+?>
