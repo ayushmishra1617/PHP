61422c4f4871e3a8c4e4e08305d09bb499394843
Ayush Mishra <145481735+ayushmishra1617@users.noreply.github.com>
2025-06-28 00:12:13 +0530

    Create string.php

---
+++ string.php
@@ -0,0 +1,20 @@
+<?php
+
+$name="Ayush Mishra";
+$num=21;
+echo "My name is ".$name;
+echo "<br>";
+echo 'My name us '.$name;//in single quate you must have to use dot(.) operator
+//it can be used join variable and constand
+echo "</br>";
+echo 'My name is '.$name. ' and age is '.$num; 
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
+$str1="Shri Ji";
+$str2="Hamari pyari Radha rani hain";
+$str1.=$str2;//concatination 
+echo $str1;
+
+?>
