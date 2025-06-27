54a238db963e8a40366ff4d56200e2d660d44f8b
Ayush Mishra <145481735+ayushmishra1617@users.noreply.github.com>
2025-06-28 01:26:55 +0530

    Create switch.php

---
+++ switch.php
@@ -0,0 +1,21 @@
+<?php
+
+$favcolor="blue";
+
+
+switch($favcolor){
+    case "red":
+        echo "My fav Color";
+        break;
+    case "blue":
+        echo "My Friends Fav";
+        break;
+    case "pink":
+        echo "brothers fav";
+        break;
+    default:
+    echo "No ones fav";
+    break;
+}
+
+?>
