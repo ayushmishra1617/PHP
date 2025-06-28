d4d0d14d8a12466d7e5663619e27155551817e5a
Ayush Mishra <145481735+ayushmishra1617@users.noreply.github.com>
2025-06-28 16:21:25 +0530

    Create func-with-return.php

---
+++ func-with-return.php
@@ -0,0 +1,9 @@
+
+<?php
+
+function name(){
+    return "Ayush Mishra";//here if use echo it will return Ayush Mishra then inside h1 tag only
+}
+echo "<h1> User name is ".name()."</h1>"
+//return is used for the output
+?>
